80c1a231250d4e69c9f8b73da7c9c8ace74af944
limdoyeon <dlaehdus126@gaeasoft.co.kr>
2021-07-26 10:18:08 +0900

    [2021년 KPI] - 수학함수 예제 구현

--- app/src/main/java/com/example/rxjavaexample/activity/MainActivity.java
+++ app/src/main/java/com/example/rxjavaexample/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rxjavaexample.activity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,10 +11,16 @@ import androidx.databinding.DataBindingUtil;
 import com.example.rxjavaexample.R;
 import com.example.rxjavaexample.databinding.ActivityMainBinding;
 
+import hu.akarnokd.rxjava2.math.MathFlowable;
+import io.reactivex.Flowable;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Single;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private ActivityMainBinding binding;
 
+    @SuppressLint("CheckResult")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +30,35 @@ public class MainActivity extends AppCompatActivity {
         binding.queryButton.setOnClickListener(onClickListener);
         binding.heartBeatButton.setOnClickListener(onClickListener);
         binding.electricBillsButton.setOnClickListener(onClickListener);
+
+        /**
+         * 수학함수
+         */
+        Integer[] data = {1, 2, 3, 4};
+        // count
+        Single<Long> source = Observable.fromArray(data)
+                .count();
+        source.subscribe(count -> Log.i(TAG, "count : " + count));
+
+        // max
+        Flowable.fromArray(data)
+                .to(MathFlowable::max)
+                .subscribe(max -> Log.i(TAG, "max : " + max));
+
+        // min
+        Flowable.fromArray(data)
+                .to(MathFlowable::min)
+                .subscribe(min -> Log.i(TAG, "min : " + min));
+
+        // sum
+        Flowable<Integer> sumFlowable = Flowable.fromArray(data)
+                .to(MathFlowable::sumInt);
+        sumFlowable.subscribe(sum -> Log.i(TAG, "sum : " + sum));
+
+        // average
+        Flowable<Double> averageFlowable = Flowable.fromArray(data)
+                .to(MathFlowable::averageDouble);
+        averageFlowable.subscribe(average -> Log.i(TAG, "average : " + average));
     }
 
     private View.OnClickListener onClickListener = v -> {
